39bf68d2925e7730bb716ae3c011895efdc390e5
moseslucas <mo@moseslucas.com>
2017-08-25 17:26:47 +0800

    Created layout for templates like header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,15 @@
+import React, {PropTypes} from 'react';
+import {Link, IndexLink} from 'react-router';
+
+const Header = (props)=> {
+  return(
+    <nav className="navbar navbar-default">
+      <ul className="nav navbar-nav">
+        <li><IndexLink to="/" activeClassName="active">Home </IndexLink></li>
+        <li><Link to="/about" activeClassName="active">About </Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
